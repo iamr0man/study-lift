370954eb82fe2bfaffb60fa5c349c66436c16d7b
Roman Korniienko <roman.korniienko@belkins.io>
2023-03-15 08:36:53 +0200

    chore: configure ts and dependency;

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import svgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8080,
+  },
   plugins: [vue(), svgLoader()],
   resolve: {
     alias: {
